2c72f9d4a518dd7518f13ab3accbcc996e2994ef
Michael Conrad <mike@nrdvana.net>
2023-03-23 16:16:00 -0400

    Expand concept of fields with defaults, and re-think timestamp
    The timestamp is now encoded as an offset from the previous
    record, and may default to 0 allowing it to be encoded as
    simply a TAB if the message happens at the same moment as
    the previous.  Also name the column timestamp_step_hex to
    help self-document it, and leave room for future extensions.
    Also make the public accessor always return the unix epoch
    and hide the implementation details of offsets or steps.

--- perl/lib/HydraLog/LogReader.pm
+++ perl/lib/HydraLog/LogReader.pm
@@ -14,11 +14,13 @@ use Carp;
 
 The HydraLog logfiles begin with:
 
-  #!hydralog-dump --format=tsv
-  # start_epoch=$dt	ts_scale=1	$name=$value ...
-  # Timestamp	Level	Message
-  $TIMESTAMP	$LEVEL	$MESSAGE
-  $TIMESTAMP	$LEVEL	$MESSAGE
+  #!hydralog-dump --format=tsv0
+  # start_epoch=$dt	timestamp_scale=1	$name=$value ...
+  # timestamp_step_hex=0	level=I	message	CustomField=Y
+  $TIMESTEP	$LEVEL	$MESSAGE
+  $TIMESTEP	$LEVEL	$MESSAGE
+  $TIMESTEP	$LEVEL	$MESSAGE	$CustomField
+  $TIMESTEP	$LEVEL	$MESSAGE
 
 In short, this tells us the file format (while self-documenting how to process the file)
 various attributes of the file, a list of the fields that will appear in each row,
@@ -61,13 +63,22 @@ sub BUILD {
 		} split /\t/, substr($line1,2);
 		defined $meta{start_epoch} or croak "Metadata doesn't list start_epoch";
 		$self->_set_start_epoch(delete $meta{start_epoch});
-		$self->_set_ts_scale(delete $meta{ts_scale} || 1);
+		$self->_set_timestamp_scale(delete $meta{ts_scale} || 1);
 		$self->_set_custom_attrs(\%meta);
 		my $line2= $self->fh->getline or croak "Can't read header line";
 		chomp $line2;
-		$self->_set_fields([ split /\t/, substr($line2,2) ]);
-		/^\w+$/ or croak "Invalid field name '$_' in header"
-			for @{ $self->fields };
+		my @fields;
+		my %defaults;
+		for my $fieldspec (split /\t/, substr($line2,2)) {
+			$defaults{$fieldspec}= $1 if $fieldspec =~ s/=(.*)//;
+			$fieldspec =~ /^\w+$/ or croak "Invalid field name '$_' in header";
+			push @fields, $fieldspec;
+		}
+		@fields > 0 && $fields[0] eq 'timestamp_step_hex'
+			or croak "First column must be timestamp_step_hex";
+		$self->_set_fields(\@fields);
+		$self->_set_field_defaults(\%defaults);
+		$self->_cur_ts(0);
 	}
 	else {
 		croak "Unknown format '".$self->format."'";
@@ -92,13 +103,30 @@ sub BUILD {
 
 =cut
 
-has filename     => ( is => 'ro' );
-has fh           => ( is => 'rwp' );
-has 'format'     => ( is => 'rwp' );
-has fields       => ( is => 'rwp' );
-has start_epoch  => ( is => 'rwp' );
-has ts_scale     => ( is => 'rwp' );
-has custom_attrs => ( is => 'rwp' );
+has filename        => ( is => 'ro' );
+has fh              => ( is => 'rwp' );
+has 'format'        => ( is => 'rwp' );
+has fields          => ( is => 'rwp' );
+has field_defaults  => ( is => 'rwp' );
+has start_epoch     => ( is => 'rwp' );
+has timestamp_scale => ( is => 'rwp' );
+has custom_attrs    => ( is => 'rwp' );
+has level_alias     => ( is => 'lazy' );
+has _cur_ts         => ( is => 'rw' );
+
+sub _build_level_alias {
+	{
+		EM => 'EMERGENCY',
+		A  => 'ALERT',
+		C  => 'CRITICAL',
+		E  => 'ERROR',
+		W  => 'WARNING',
+		N  => 'NOTICE',
+		'' => 'INFO',
+		D  => 'DEBUG',
+		T  => 'TRACE',
+	}
+}
 
 =head1 METHODS
 
@@ -117,8 +145,16 @@ sub next {
 	redo if ord $line == COMMENT || !length $line;
 	my %rec;
 	@rec{ @{$self->fields} }= split /\t/, $line;
-	$rec{timestamp}= hex $rec{timestamp} if $rec{timestamp};
-	$rec{_log_reader}= $self;
+	for (keys %{$self->field_defaults}) {
+		$rec{$_}= $self->field_defaults->{$_} unless defined $rec{$_} && length $rec{$_};
+	}
+	if (defined $rec{timestamp_step_hex}) {
+		my $t= ($self->{_cur_ts} += hex $rec{timestamp_step_hex});
+		$rec{timestamp}= $t / $self->timestamp_scale + $self->start_epoch;
+	}
+	my $l;
+	$rec{level}= $l
+		if defined $rec{level} && defined ($l= $self->level_alias->{$rec{level}});
 	bless \%rec, 'HydraLog::LogReader::Record';
 }
 
@@ -132,6 +168,58 @@ Reading the C<timestamp> returns the same integer that was read from the file (b
 decoded from hex).  There is a virtual field C<epoch> which scales and adds the timestamp
 to the C<start_epoch> of the log file.
 
+=head2 ATTRIBUTES
+
+=over
+
+=item C<timestamp>
+
+Unix epoch number, which may contain a fraction if the timestamps are sub-second precision.
+
+=item C<timestamp_local>
+
+The timestamp, converted to local time zone in C<< YYYY-MM-DD HH:MM::SS[.x] >> format.
+
+=item C<timestamp_iso8601>
+
+The timestamp, in ISO-8601 C<< YYYY-MM-DDTHH:MM:SS[.x]Z >> format.
+
+=item C<level>
+
+Log level, which can be any string, but is usually normalized to one of:
+
+  EMERGENCY
+  ALERT
+  CRITICAL
+  ERROR
+  WARNING
+  NOTICE
+  INFO
+  DEBUG
+  TRACE
+
+=item C<facility>
+
+The syslog facility name.  This is a string, not guaranteed to match any of the constants
+on your local C library.
+
+=item C<identity>
+
+The program name that generated the message.  In messages from syslog, this is the
+portion before ':' in the logged string.
+
+=item C<message>
+
+The message text.
+
+=item C<*>
+
+Any other field whose name is composed of C<< /\w+/ >> can be accessed as an attribute, but
+you get an exception if the field wasn't defined for this log.  This object does not have
+any private fields, so you may access C<< ->{$field} >> rather than using an accessor.
+
+=back
+
 =cut
 
 package HydraLog::LogReader::Record {
@@ -140,41 +228,36 @@ package HydraLog::LogReader::Record {
 	use overload '""' => sub { $_[0]->to_string };
 
 	sub timestamp { $_[0]{timestamp} }
+	sub level     { $_[0]{level} }
+	sub facility  { $_[0]{facility} }
+	sub identity  { $_[0]{identity} }
+	sub message   { $_[0]{message} }
 
-	sub timestamp_epoch {
-		return undef unless defined(my $ts= $_[0]{timestamp});
-		$ts / $_[0]{_log_reader}->ts_scale + $_[0]{_log_reader}->start_epoch;
-	}
-
-	sub timestamp_localtime {
-		my $epoch= $_[0]->timestamp_epoch or return undef;
+	sub timestamp_local {
+		my $epoch= $_[0]->timestamp or return undef;
 		my ($sec, $min, $hour, $mday, $mon, $year)= localtime $epoch;
 		return sprintf "%04d-%02d-%02d %02d:%02d:%02d%s",
 			$year+1900, $mon+1, $mday, $hour, $min, $sec,
 			($epoch =~ /(\.\d+)$/? $1 : '');
 	}
 
-	sub timestamp_gmtime {
-		my $epoch= $_[0]->timestamp_epoch or return undef;
+	sub timestamp_iso8601 {
+		my $epoch= $_[0]->timestamp or return undef;
 		my ($sec, $min, $hour, $mday, $mon, $year)= gmtime $epoch;
 		return sprintf "%04d-%02d-%02dT%02d:%02d:%02d%sZ",
 			$year+1900, $mon+1, $mday, $hour, $min, $sec,
 			($epoch =~ /(\.\d+)$/? $1 : '');
 	}
 
-	sub level     { $_[0]{level} }
-	sub facility  { $_[0]{facility} }
-	sub identity  { $_[0]{identity} }
-	sub message   { $_[0]{message} }
-
+		
 	sub to_string {
 		my $self= shift;
 		return join ' ',
 			(defined $self->{timestamp}? ( $self->timestamp_localtime ) : ()),
-			(defined $self->{log_level}? ( $self->log_level ) : ()),
-			(defined $self->{facility}? ( $self->facility ) : ()),
-			(defined $self->{identity}? ( $self->identity ) : ()),
-			(defined $self->{message}? ( $self->message ) : ());
+			(defined $self->{level}?     ( $self->level_name ) : ()),
+			(defined $self->{facility}?  ( $self->facility ) : ()),
+			(defined $self->{identity}?  ( $self->identity.':' ) : ()),
+			(defined $self->{message}?   ( $self->message ) : ());
 	}
 	
 	sub AUTOLOAD {
--- perl/t/10-log-reader.t
+++ perl/t/10-log-reader.t
@@ -4,7 +4,7 @@ use HydraLog::LogReader;
 my $reader= HydraLog::LogReader->new(filename => \<<END);
 #!hydralog-dump --format=tsv0
 # start_epoch=1577836800	ts_scale=1	example=true
-# timestamp	level	message
+# timestamp_step_hex=0	level	message
 0	INFO	Testing 1
 10	WARN	Testing 2
 END
@@ -13,18 +13,17 @@ like( $reader,
 	object {
 		call 'format' => 'tsv0';
 		call start_epoch => 1577836800;
-		call ts_scale => 1;
+		call timestamp_scale => 1;
 		call custom_attrs => { example => 'true' };
-		call fields => [qw( timestamp level message )],
+		call fields => [qw( timestamp_step_hex level message )],
 	},
 	'reader object'
 );
 
 like( $reader->next,
 	object {
-		call timestamp => 0;
-		call timestamp_epoch => 1577836800;
-		call timestamp_gmtime => '2020-01-01T00:00:00Z';
+		call timestamp => 1577836800;
+		call timestamp_iso8601 => '2020-01-01T00:00:00Z';
 		call level => 'INFO';
 		call message => 'Testing 1';
 	},
@@ -32,9 +31,8 @@ like( $reader->next,
 );
 like( $reader->next,
 	object {
-		call timestamp => 16;
-		call timestamp_epoch => 1577836800+16;
-		call timestamp_gmtime => '2020-01-01T00:00:16Z';
+		call timestamp => 1577836800+16;
+		call timestamp_iso8601 => '2020-01-01T00:00:16Z';
 		call level => 'WARN';
 		call message => 'Testing 2';
 	},
